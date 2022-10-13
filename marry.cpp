f03f4fbf006d0340ae7f8605469de6e2bb564d60
wait-work <115733778+wait-work@users.noreply.github.com>
2022-10-13 23:05:40 +0530

    Create marry.cpp
    this is my first project named "unescapable marriage" I hope u all like it : )

---
+++ marry.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <string> 
+# include <cmath>
+using namespace std;
+
+int main(){
+cout<<"will u marry me ?"<<"\n";
+string x;
+cin>>x;
+while(x!="yes"){
+    cout<<"wrong choice"<<"\n";
+    cin>>x;
+}
+  return 0;
+}
